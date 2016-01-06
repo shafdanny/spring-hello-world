3974efe71276f5f9f93fcc75d828efc0a784a0cf
Shafiq Daniel <shafdanny@gmail.com>
2016-01-06 20:37:43 +0100

    First commit

---
+++ src/hello/Application.java
@@ -0,0 +1,15 @@
+package hello;
+
+/**
+ * Created by shafiq on 06/01/16.
+ */
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
